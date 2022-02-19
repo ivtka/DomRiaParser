6893c936ea5be6bbd9b208b64fa759656ed3e618
Ivan Tkachuk <ivtkac@gmail.com>
2022-02-19 02:01:55 +0200

    Telegram Bot

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 DOMRIA_URL = "https://dom.ria.com/uk/prodazha-kvartir"
 BASE_URL = "https://dom.ria.com/"
 TOKEN = "5127659598:AAEYYIMkMJWQXgIEbJmJnb9_mgrdgNvVHqA"
-DRIVER_PATH = "./chromedriver"
 CITIES = {'Чернівці': ['Квартал Вілл', 'Ніжин', 'Першотравневий',
                        'Садгорський', 'Шевченківський'], 'Івано-Франківськ': ['Івасюка Надрічна', 'Арсенал', 'Бам']}
+GOOGLE_CHROME_BIN = "/app/.apt/usr/bin/google-chrome"
+CHROMEDRIVER_PATH = "/app/.chromedriver/bin/chromedriver"
