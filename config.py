a2b87f77c8cd0e0f1499d48e8ea70716337bc6f3
Ivan Tkachuk <ivtkac@gmail.com>
2022-02-19 03:52:45 +0200

    Heroku

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ BASE_URL = "https://dom.ria.com/"
 TOKEN = "5127659598:AAEYYIMkMJWQXgIEbJmJnb9_mgrdgNvVHqA"
 CITIES = {'Чернівці': ['Квартал Вілл', 'Ніжин', 'Першотравневий',
                        'Садгорський', 'Шевченківський'], 'Івано-Франківськ': ['Івасюка Надрічна', 'Арсенал', 'Бам']}
-GOOGLE_CHROME_BIN = "/usr/bin/google-chrome-stable"
-CHROMEDRIVER_PATH = "./chromedriver"
+GOOGLE_CHROME_BIN = "/app/.apt/usr/bin/google-chrome"
+CHROMEDRIVER_PATH = "/app/.chromedriver/bin/chromedriver "
